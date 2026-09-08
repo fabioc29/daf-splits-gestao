ab5dda70474145f1cb5127554103d7eba08d11ac
Gabriel Rodrigo <93385461+Gabriel-GR1@users.noreply.github.com>
2026-09-07 22:41:36 -0300

    chore: test automatic Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+// Deploy connectivity check: no visual or behavioral effect.
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
